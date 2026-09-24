2040c12dd22eb0aaaa87cda3de4a10c29c8cf581
Gabreil Chilando <Chilandogabriel21@gmail.com>
2026-09-24 21:06:12 +0200

    first update

--- src/main/java/com/example/hellofx/HelloJavaFX.java
+++ src/main/java/com/example/hellofx/HelloJavaFX.java
@@ -13,7 +13,7 @@ public class HelloJavaFX extends Application {
     @Override
     public void start(Stage stage) {
 
-        Label message = new Label("Welcome, Rodrick Lungoh!");
+        Label message = new Label("Welcome, Gabriel Chilando!");
 
         Button button = new Button("Start");
         Button resetButton = new Button("Reset");
@@ -21,7 +21,7 @@ public class HelloJavaFX extends Application {
         button.setOnAction(event ->
                 message.setText("Great! You clicked the button.")
         );
-        resetButton.setOnAction(e-> message.setText("Welcome, Rodrick Lungoh!X!"));
+        resetButton.setOnAction(e-> message.setText("Welcome, Gabriel Chilando!"));
 
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.CENTER);
@@ -29,7 +29,7 @@ public class HelloJavaFX extends Application {
 
         Scene scene = new Scene(layout, 500, 300);
 
-        stage.setTitle("My First JavaFX Application- Student Number: 202503470");
+        stage.setTitle("My First JavaFX Application- Student Number: 202505771");
         stage.setScene(scene);
         stage.show();
     }
